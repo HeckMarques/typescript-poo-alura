fec5a81fa7e2eb4b1b0e66fba36a310163ce6b4d
heckmarques <mauricio.marques888@gmail.com>
2023-06-21 14:54:10 -0300

    Curso finalizado

---
+++ app/src/app.ts
@@ -0,0 +1,24 @@
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+
+const controller = new NegociacaoController();
+
+const form = document.querySelector(".form");
+if (form) {
+  form.addEventListener("submit", (event: Event) => {
+    event.preventDefault();
+    controller.adiciona();
+  });
+} else {
+  throw new Error(
+    "Não foi possível inicializar a aplicação. Verifique se o form existe"
+  );
+}
+
+const botaoImporta = document.querySelector("#botao-importa");
+if (botaoImporta) {
+  botaoImporta.addEventListener("click", () => {
+    controller.importarDados();
+  });
+} else {
+  throw new Error("Botão importa não foi encontrado");
+}
